1fd7801b7952b57a15e57f267a56046f3e12fa00
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-05-23 21:35:11 -0400

    add starter xcode project

---
+++ Comics/Comics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comics
+//
+//  Created by Julian A. Fordyce on 5/23/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
